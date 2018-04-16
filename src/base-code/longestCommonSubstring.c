4861757484fa5486f55569e193e71267de2c553d
Ryan Williams <rpwilliams96@selene.beocat.ksu.edu>
2018-04-15 22:02:33 -0500

    Cleaned up max substring code

--- src/base-code/longestCommonSubstring.c
+++ src/base-code/longestCommonSubstring.c
@@ -3,81 +3,69 @@
 #include <string.h>
 
 void max_substring(char* str1, char* str2, int m, int n);
-void print_max_substring(char* biggest, int start);
 char *strrev(char *str);
 
-
 void main() {
-	char* str1 = "999999999999999999999999abccccccccccccccccccccccccccccccccccccccccccccccc1111111111111111111111111110000000000000 fffffffffffp ";
-	char* str2 = "abc2222222222222222222222222222222222222222222444444444444444444444";
+	// char* str1 = "fkfokoabcccccc";
+	// char* str2 = "abccccccokojfifeiwh";
+	char* str1 = "thisabcabacabcabcabcisatest";
+	char* str2 = "testing123testing123abcabacabcabcabc";
 	max_substring(str1, str2, strlen(str1), strlen(str2));
-	// printf(max_substring(str1, str2, strlen(str1), strlen(str2)), "%s\n");
 }
 
-/* Returns the biggest of a and b */
-int max(int a, int b) {
-	if(a >= b) {
-		return a;
-	}
-	return b;
-}
-
+/* Find and prints the biggest AND most common substring between 2 str1 and str2.
+   Code inspired by https://www.geeksforgeeks.org/longest-common-substring/ */
 void max_substring(char* str1, char* str2, int m, int n) {
-	/* Allocate space for the biggest substring */
-	int MAX_SUBSTRING_SIZE = max(m, n) + 1;
-	char biggest[10];
-	int table[m+1][n+1];
+	char biggest[100]; // The biggest/most common substring
+	int table[m+1][n+1]; // 2D array to calculate biggest/most common substring
 	
-	int longest_i = 0;
-	int longest_j = 0;
-	int i, j = 0;
-	int len = 0;
+	int row, col = 0;
+	int biggest_row, biggest_col = 0; // Index of the LAST letter of the biggest substring we've found
+	int max_len = 0; // The length of the biggest substring we've found
+
 	/* Populate the table */
-	for(i = 0; i <= m; i++) {
-		for(j = 0; j <= n; j++) {
-			/* If the letters are the same, add 1 to the left diagonal  
+	for(row = 0; row <= m; row++) {
+		for(col = 0; col <= n; col++) {
+			/* Initialize table[1..m][0] and table[0][1..n] to 0 */
+			if(row == 0 || col == 0) {
+				table[row][col] = 0;
+			}
+			/* If the letters of the two strings are the same, add 1 to the left diagonal  
 			   value and set it to the current position */
-			if(str1[i] == str2[j]) {
-				table[i][j] = table[i-1][j-1] + 1;
+			else if(str1[row] == str2[col]) {
+				table[row][col] = table[row-1][col-1] + 1;
 				
-				if(max(table[i][j], table[longest_i][longest_j]) == table[i][j]) {
-					len = table[i][j];
-					longest_i = i;
-					longest_j = j;
+				/* If the current position is bigger than the biggest substring
+				   we've found so far, update our variables so that we can find
+				   it later on. */
+				if(table[row][col] > max_len) {
+					max_len = table[row][col];
+					biggest_row = row;
+					biggest_col = col;
 				}
 			}
 			/* If the letters are not the same, the substring length is 0 */
 			else {
-				table[i][j] = 0;				
+				table[row][col] = 0;				
 			}
 		}
 	}
 
-	/* Iterate through the table to build biggest substring */
-	i = longest_i;
-	j = longest_j;
-	// biggest[len] = '\0';
-	// len--;
-	while(table[i][j] != 0) {
-		// biggest[len] = str1[i];
-		// len--;
-		sprintf(biggest + strlen(biggest), "%c", str1[i]);
-		i--;
-		j--;
+	/* Starting at the bottom right of the biggest substring in the table, build biggest substring */
+	while(table[biggest_row][biggest_col] != 0) {
+		/* Convert the letter to a string, since strcat requires a string */
+		char temp[10];
+		temp[0] = str1[biggest_row];	
+		strcat(biggest, temp);
+		
+		/* Move to the next letter (top right) in the backwards substring */
+		biggest_row--;
+		biggest_col--;
 	}
 
+	/* Print and reverse the biggest substring */
 	printf("%s", strrev(biggest));
-	// print_max_substring(biggest, len);
-	// printf("%s", biggest);
-
 } 
-
-void print_max_substring(char biggest[], int start) {
-	int i;
-	for(i = start; i < strlen(biggest); i++) {
-		printf("%c", biggest[i]);
-	}
-}
 
 /* Reverse a string
    From https://stackoverflow.com/questions/8534274/is-the-strrev-function-not-available-in-linux */
