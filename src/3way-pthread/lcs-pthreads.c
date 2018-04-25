0ea9424dc423b47ca20eba773337108861fa2b7e
Mark Spicer <mspicer60@selene.beocat.ksu.edu>
2018-04-25 12:45:35 -0500

    Began working on pthreads

---
+++ src/3way-pthread/lcs-pthreads.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+/* Constants */
+#define NUM_ENTRIES 1000000 // Should be 1000000
+#define NUM_THREADS 8
+#define LINE_LENGTH 2003 //should be 2003, increasing to this size causes a segmentation fualt due to size of table.
+
+/* Global Variables */
+char entries[NUM_ENTRIES][LINE_LENGTH];
+pthread_mutex_t mutexsum;			// mutex for char_counts
+
+/* Function prototypes */
+void max_substring(int myID);
+char *strrev(char *str);
+void read_file();
+
+void main() {
+	int i, rc;
+	/* Create variables needed for pthreads */
+	pthread_t threads[NUM_THREADS];
+	pthread_attr_t attr;
+	void *status;
+	/* Initialize and set thread detached attribute */
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	
+	/* Read the file into the the list of entries */
+	read_file();
+	
+	for (i = 0; i < NUM_THREADS; i++ ) {
+	  rc = pthread_create(&threads[i], &attr, max_substring, (void *)i);
+	  if (rc) {
+		printf("ERROR; return code from pthread_create() is %d\n", rc);
+	exit(-1);
+	  }
+	}
+	
+	/* Free attribute and wait for the other threads */
+	pthread_attr_destroy(&attr);
+	for(i=0; i<NUM_THREADS; i++) {
+	     rc = pthread_join(threads[i], &status);
+	     if (rc) {
+		   printf("ERROR; return code from pthread_join() is %d\n", rc);
+		   exit(-1);
+	     }
+	}
+	
+	pthread_mutex_destroy(&mutexsum);
+	pthread_exit(NULL);
+}
+
+/* Read the file from wiki_dump.txt into the list of entries */
+void read_file() {
+	pthread_mutex_init(&mutexsum, NULL);
+	FILE *fp;
+	char str1[LINE_LENGTH];
+	fp = fopen("/homes/dan/625/wiki_dump.txt", "r");
+	// fp = fopen("test.txt", "r");
+
+	/* If the file could not be found, return */
+	if(fp == NULL) {
+		perror("Failed: ");
+		return;
+	}
+	
+	/* Add each line of the file into entries */
+	int i = 0;
+	while(fgets(str1, LINE_LENGTH, fp) != NULL && i < NUM_ENTRIES){
+		strcpy(entries[i], str1);
+		i++;
+	}
+
+	fclose(fp);
+}
+
+/* Find and prints the biggest AND most common substring between 2 str1 and str2.
+   Code inspired by https://www.geeksforgeeks.org/longest-common-substring/ */
+void max_substring(int myID) {
+	int startPos = myID * (NUM_ENTRIES / NUM_THREADS);
+	int endPos = startPos + (NUM_ENTRIES / NUM_THREADS);
+	
+	char str1[LINE_LENGTH];
+	char str2[LINE_LENGTH];
+	int m, n, i;
+	char biggest[LINE_LENGTH]; // The biggest/most common substring
+	char temp[LINE_LENGTH];
+	
+	int row, col;
+	int biggest_row, biggest_col; // Index of the LAST letter of the biggest substring we've found
+	int max_len; // The length of the biggest substring we've found
+
+	
+		for(i = startPos; i < endPos; i++)
+		{
+			strcpy(str1, entries[i]);
+			strcpy(str2, entries[i+1]);
+			m = strlen(str1);
+			n = strlen(str2);
+
+			/* Allocate memory for a table */
+			int (*table)[n] = malloc(sizeof(int[m + 1][n + 1]));
+			
+			max_len = 0;
+			row =  0; 
+			col = 0;
+			biggest_row = 0;
+			biggest_col = 0;
+			
+			/* Populate the table */
+			for(row = 0; row < m; row++) {
+				for(col = 0; col < n; col++) {
+					/* Initialize table[1..m][0] and table[0][1..n] to 1 */
+					if(row == 0 || col == 0) {
+						table[row][col] = 1;
+					}
+					/* If the letters of the two strings are the same, add 1 to the left diagonal  
+					   value and set it to the current position */
+					else if(str1[row] == str2[col] && str1[row] != '\n'){
+						table[row][col] = table[row-1][col-1] + 1;
+						
+						/* If the current position is bigger than the biggest substring
+						   we've found so far, update our variables so that we can find
+						   it later on. */
+						if(table[row][col] > max_len) {
+							max_len = table[row][col];
+							biggest_row = row;
+							biggest_col = col;
+						}
+					}
+					/* If the letters are not the same, the substring length is 0 */
+					else {
+						table[row][col] = 0;				
+					}
+				}
+			}
+
+			if(max_len == 0) {
+				printf("%d-%d:, %s\n", i, i+1, "No common substring.");
+			}
+			else{
+				memset(biggest, '\0', LINE_LENGTH);
+				/* Starting at the bottom right of the biggest substring in the table, build biggest substring */
+				while(table[biggest_row][biggest_col] != 1) {
+				 	/* Convert the letter to a string, since strcat requires a string */				
+					memset(temp, '\0', LINE_LENGTH);
+					
+					temp[0] = str1[biggest_row];
+	
+				 	/* Concatonate the string */
+					strcat(biggest, temp);
+					
+				 	/* Move to the next letter (top left) in the backwards substring */
+					biggest_row--;
+					biggest_col--;
+				}
+				strcat(biggest, "\0");
+
+				/* Print and reverse the biggest substring */
+				printf("%d-%d: %s\n", i, i+1, strrev(biggest));
+			}
+
+			/* Free the table */
+			free(table);
+		}
+} 
+
+/* Reverse a string
+   From https://stackoverflow.com/questions/8534274/is-the-strrev-function-not-available-in-linux */
+char *strrev(char *str)
+{
+      char *p1, *p2;
+
+      if (! str || ! *str)
+            return str;
+      for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+      {
+            *p1 ^= *p2;
+            *p2 ^= *p1;
+            *p1 ^= *p2;
+      }
+      return str;
+}
---
+++ src/3way-pthread/mass-sbatch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 1 2 4 8 16
+do
+	 sbatch --job-name=testkill --ntasks-per-node=$i --nodes=1 --mem-per-cpu=$((128/$i))G --partition=killable.q pthread-sbatch.sh
+done
