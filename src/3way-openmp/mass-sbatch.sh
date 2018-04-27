ab6ad489fc5bff9d21443958c5c35a612ef4df7e
Ryan Williams <rpwilliams96@selene.beocat.ksu.edu>
2018-04-26 19:50:41 -0500

    Changed job directories

--- src/3way-openmp/mass-sbatch.sh
+++ src/3way-openmp/mass-sbatch.sh
@@ -2,5 +2,8 @@
 
 for i in 1 2 4 8 16 
 do
-	 sbatch --constraint=elves --time=04:00:00 --ntasks-per-node=$i --nodes=1 --mem-per-cpu=$((16/$i))G --partition=killable.q openmp-sbatch.sh
+	for j in 1 2 3 4 5 6 7 8 9 10
+	do
+		sbatch --constraint=elves --time=04:00:00 --ntasks-per-node=$i --nodes=1 --mem-per-cpu=$((16/$i))G --partition=killable.q openmp-sbatch.sh
+	done
 done
--- src/3way-openmp/openmp-sbatch.sh
+++ src/3way-openmp/openmp-sbatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-//usr/bin/time -o /homes/rpwilliams96/CIS520_Proj4/src/3way-openmp/time-10k/time-$RANDOM.txt /homes/rpwilliams96/CIS520_Proj4/src/3way-openmp/output-10k/openmp-10k
+//usr/bin/time -o /homes/rpwilliams96/CIS520_Proj4/src/3way-openmp/time-100k/time-$RANDOM.txt /homes/rpwilliams96/CIS520_Proj4/src/3way-openmp/output-10k/openmp-10k
